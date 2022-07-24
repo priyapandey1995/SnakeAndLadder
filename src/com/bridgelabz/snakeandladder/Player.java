4478d14f2004f1b20877984fda4d7c8f5c5f7d91
priya <priyapandey8884.blr@gmail.com>
2022-07-24 23:27:18 +0530

    [Priya]:Added Players Position At 0

---
+++ src/com/bridgelabz/snakeandladder/Player.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.snakeandladder;
+/*
+ * 
+ */
+public class Player {
+	/*
+	 * 
+	 */
+	public void toAssignPosition() {
+		/*Assigning playerposition at 0
+		 * 
+		 */
+		int playerPosition =0;
+		System.out.println("Single player position starts at"+playerPosition);
+	}
+
+}
---
+++ src/com/bridgelabz/snakeandladder/SnakeLadder.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.snakeandladder;
+
+public class SnakeLadder {
+
+	public static void main(String[] args) {
+		/*
+		 * object creation for class player
+		 * method invocation
+		 * 
+		 */
+		System.out.println("Welcome to the Sanke and ladder program");
+		Player play = new Player();
+		play.toAssignPosition();
+	}
+
+}
