b87ad394040e645adbc3533afbcd16e731fdd279
priya <priyapandey8884.blr@gmail.com>
2022-07-28 01:57:46 +0530

    [Priya]:Added Two player Game

--- src/com/bridgelabz/snakeandladder/Player.java
+++ src/com/bridgelabz/snakeandladder/Player.java
@@ -12,7 +12,9 @@ public class Player {
 	static final int winningPosition = 100;
 
 	public void toRollADice() {
-		/**here,the player rolls the dice to get the number
+		
+		/**two player are initialized ,
+		 * here,both the player rolls the dice to reach till 100 position
 		 *hence,players position gets updated with newly generated random number
 		 *
 		 *generate the random number to check for the option
@@ -23,54 +25,82 @@ public class Player {
 		 * checks for the exact winning position
 		 * count variable is initialized to check the number of times the dice is played
 		 * 
+		 * 
 		 */
 		
-		int playerPosition =0;
-		int count = 0;
+		int playerOnePosition =0;
+		int playerTwoPosition=0;
+		int diceRollingOne,diceRollingTwo;
+		int count_player_1 = 0;
+		int count_player_2=0;
 		
-		System.out.println("Initial position of the player is :"+  playerPosition);
+		System.out.println("Initial position of the player is :"+  playerOnePosition + "and"+playerTwoPosition);
 		
-		while(playerPosition!=winningPosition) {
-		
-			 count++;
-			
-		int diceRolling = (int)(Math.floor((Math.random()*6)+1));
+		while(playerOnePosition!=winningPosition && playerTwoPosition!=winningPosition) {
+	    count_player_1++;
+		diceRollingOne = (int)(Math.floor((Math.random()*6)+1));
 		
 		int optionCheck = (int)(Math.floor(Math.random()*3));
 		switch(optionCheck) {
 			case 0:System.out.println("So its No Play");
-			System.out.println("Players position would be :"+playerPosition);
+			System.out.println("Players position would be :"+playerOnePosition);
 			break;
 			
 			case 1:System.out.println("its a Ladder");
-			playerPosition= playerPosition+diceRolling;
-			if(playerPosition>winningPosition) {
-			playerPosition = playerPosition-diceRolling;
-			System.out.println("Players position would be :" + playerPosition);
+			playerOnePosition= playerOnePosition+diceRollingOne;
+			if(playerOnePosition>winningPosition) {
+			playerOnePosition = playerOnePosition-diceRollingOne;
+			System.out.println("Players position would be :" + playerOnePosition);
 			}
 		    break;
 			
 			case 2:System.out.println("its a Snake");
-			playerPosition = playerPosition - diceRolling;
-			if(playerPosition< 0) {
-				playerPosition = 0;
+			playerOnePosition = playerOnePosition - diceRollingOne;
+			if(playerOnePosition< 0) {
+				playerOnePosition = 0;
 				break;
 			}
 		}
 			
-			System.out.println("player position is "+playerPosition);
-			if(playerPosition == winningPosition)
-			{
-				System.out.println("it has reached to the winning position");
-				System.out.println("the total no of time the dice was played to win the game"+ " "+count);
-			}
+			System.out.println("playerOne position is "+playerOnePosition);
+		
+			/*
+			 * Second player starts to play
+			 */
+			count_player_2++;
+			System.out.println("the count for palyer 2 is"+ count_player_2);
+			diceRollingTwo = (int)(Math.floor((Math.random()*6)+1));
+			int optionCheckTwo= (int)(Math.floor(Math.random()*3));
 			
-				
-		
-		
-		
-		
+			switch(optionCheckTwo) {
+			case 0:System.out.println("So its No Play");
+			System.out.println("Players position would be :"+playerTwoPosition);
+			break;
+			
+			case 1:System.out.println("its a Ladder");
+			playerTwoPosition= playerTwoPosition+diceRollingTwo;
+			if(playerTwoPosition>winningPosition) {
+			playerTwoPosition = playerTwoPosition-diceRollingTwo;
+			System.out.println("Player Two position would be :" + playerTwoPosition);
+			}
+		    break;
+			
+			case 2:System.out.println("its a Snake");
+			playerTwoPosition = playerTwoPosition - diceRollingTwo;
+			if(playerTwoPosition< 0) {
+				playerTwoPosition = 0;
+				break;
+			}
+		}
+			System.out.println("player two position is"+playerTwoPosition);
+			
 	}
+		if(playerOnePosition ==winningPosition) {
+			System.out.println("Player one has won the game"+ "with" + " " +count_player_1 + " steps");
+		}
+		else {
+			System.out.println("Player two has won the game" + "with" +" "+ count_player_2 + "steps");
+		}
 
 		
 		
